66110499c070aa0cccef5e14a2fbde84a307fe51
Bieliakov <serhii.bieliakov@gmail.com>
2025-05-08 13:04:44 +0300

    write more tests for normal items flow

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -2,10 +2,45 @@ const {Shop, Item} = require("../src/gilded_rose");
 const { mockNomralItems } = require("./mockData");
 
 describe("Gilded Rose", function() {
-  describe("for normal items", function() {
-    it("properly decrease quality if 1 day passes", function() {
-      const gildedRose = new Shop(mockNomralItems);
+  describe("for normal items decrease quality", function() {
+    let mockNomralItemsCopy = [];
+    let gildedRose = null;
+
+    beforeEach(() => {
+      mockNomralItemsCopy = [...mockNomralItems];
+      gildedRose = new Shop(mockNomralItemsCopy);
+    });
+
+    it("if 1 day passes", function() {
       const items = gildedRose.updateQuality();
+      expect(items).toMatchSnapshot();
+    });
+    it("if 5 day passes", function() {
+      let items;
+      for (let i = 1; i < 5; i++) {
+        items = gildedRose.updateQuality();
+      }
+
+      expect(items).toMatchSnapshot();
+    });
+    it("if item's maximum item's day passed", function() {
+      const maximumDay = mockNomralItems.reduce((max, item) => Math.max(max, item.sellIn), 0);
+
+      let items;
+      for (let i = 1; i <= maximumDay; i++) {
+        items = gildedRose.updateQuality();
+      }
+
+      expect(items).toMatchSnapshot();
+    });
+    it("if more then item's maximum item's day passed", function() {
+      const maximumDay = mockNomralItems.reduce((max, item) => Math.max(max, item.sellIn), 0);
+
+      let items;
+      for (let i = 1; i <= maximumDay + 1; i++) {
+        items = gildedRose.updateQuality();
+      }
+
       expect(items).toMatchSnapshot();
     });
   });
